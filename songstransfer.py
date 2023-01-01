5f5eb8fcbb6f63c4ec1012cf470d94a9dd756127
Jose Manuel González Fornell <josemanuelgonzalezfornell@gmail.com>
2023-01-01 19:58:11 +0100

    songstransfere.py file adhere
    synchronizing songs files and playlist

--- songstransfer.py
+++ songstransfer.py
@@ -1,27 +1,120 @@
 from musiclibrary import *
-from os.path import exists
-from shutil import copy
+from os.path import exists, join
+from os import makedirs, remove, listdir
+from shutil import copy2 as copy
+from re import sub
+from io import open
 
-# Save the ubication of the .xml archive with the iTunes library to export
-libraryubication = input
+# Special characters
+specialcharacters = r"\/|\\|&|\?|!|\*|@|\$|€|%|=|~|\[|\]|{|}|<|>|\^|´|’|\.$"
+specialcharacterstitle = r"\/|\\|&|\?|!|\*|@|\$|€|%|=|~|\[|\]|{|}|<|>|\^|´|’"
 
-# Save the ubication where the library want to import
-archivefinalubication = input
-
-# Original archive ubication
-originalarchiveubication = input
-
-# iTunes library to export
-library = Library(libraryubication)
+# Synchronize origin and destination folder
 
 
-for songs in library.songs:
-    # Create a trucknumber with 2 figures
-    trucknumber = str(songs.trucknumber)
-    if len(str(songs.trucknumber)) == 0:
-        trucknumber = "0"+str(songs.trucknumber)
+def songsTransferer(librarylocation, archivefinallocation, originalarchivelocation):
 
-    # Corrovorate if the .mp3 archive exist in the final file, if not copy the archive
-    if exists(archivefinalubication+"/"+songs.artist+"/"+songs.album+""+trucknumber+songs.title+".mp3") == False:
-        copy(originalarchiveubication, archivefinalubication+"/"+songs.artist +
-             "/"+songs.album+"/"+trucknumber+songs.title+".mp3")
+    # iTunes library to export
+    library = Library(librarylocation)
+
+    for songs in library.songs:
+
+        # artist without special characters
+        artist = sub(specialcharacters, "_", songs.artist)
+        # title without special characters
+        title = sub(specialcharacterstitle, "_", songs.title)
+        # album without special characters
+        album = sub(specialcharacters, "_", songs.album)
+
+        # Create a trucknumber with 2 figures
+        tracknumber = ""
+        if songs.tracknumber < 10:
+            tracknumber += "0"
+        tracknumber += str(songs.tracknumber)
+
+        # create the folder tree
+        folder = "/" + artist + "/" + album
+
+        # create the file path
+        file = "/" + folder + "/"
+        if songs.discnumber is not None:
+            file += str(songs.discnumber)+"-"
+        file += tracknumber+" "+title+".mp3"
+
+        # Corrovorate if the .mp3 archive exist in the final file, if not copy the archive
+        if exists(archivefinallocation+file) == False:
+            if exists(archivefinallocation+folder) == False:
+                makedirs(archivefinallocation+folder)
+                copy(originalarchivelocation+file, archivefinallocation+file)
+            else:
+                copy(originalarchivelocation+file, archivefinallocation+file)
+
+# Export playlist from origin to final folder
+
+
+def playlistTransferer(librarylocation, archivefinallocation):
+
+    # list of all archive with .m3u extention in final folder
+    listplaylist = listdir(archivefinallocation)
+
+    # remove all prexistiing files with .m3u extention in final folder
+    for item in listplaylist:
+        if item.endswith(".m3u"):
+            remove(join(archivefinallocation, item))
+
+    # iTunes Library to export
+    library = Library(librarylocation)
+
+    for songs in library.songs:
+
+        # artist without special characters
+        artist = sub(specialcharacters, "_", songs.artist)
+        # title without special characters
+        title = sub(specialcharacterstitle, "_", songs.title)
+        # album without special characters
+        album = sub(specialcharacters, "_", songs.album)
+
+        # Create a trucknumber with 2 figures
+        tracknumber = ""
+        if songs.tracknumber < 10:
+            tracknumber += "0"
+        tracknumber += str(songs.tracknumber)
+
+        # create the folder tree
+        folder = "/" + artist + "/" + album
+
+        # create the file path
+        file = "/" + folder + "/"
+        if songs.discnumber is not None:
+            file += str(songs.discnumber)+"-"
+        file += tracknumber+" "+title+".mp3"
+
+        # name of song playlist
+        nameplaylist = ""
+
+        # cover all the song pllaylists
+        for x in library.playlists:
+            index = 0
+            while index <= x.getLength()-1:
+                if x.getSongs()[index].id == songs.id:
+                    nameplaylist = x.name
+                index += 1
+
+            # enter in this condiction if song belong to a playlist
+            if nameplaylist != None:
+
+                # create a .m3u file if it doesn't exist and add the song's path to this file
+                if exists(archivefinallocation + "/" + nameplaylist + ".m3u") == False:
+                    newplaylist = open(archivefinallocation + "/" +
+                                       nameplaylist + ".m3u", "a")
+                    newplaylist.write(archivefinallocation + file + "\n")
+                    newplaylist.close()
+                    nameplaylist = ""
+
+                # add the song's path to the playlist file if it's exist
+                else:
+                    newplaylist = open(archivefinallocation + "/" +
+                                       nameplaylist + ".m3u", "a")
+                    newplaylist.write(archivefinallocation + file + "\n")
+                    newplaylist.close()
+                    nameplaylist = ""
--- test_songstransfer.py
+++ test_songstransfer.py
@@ -1,7 +1,21 @@
-def test_Transfer():
-    #TODO transfer songs to a new destination folder
-    #TODO remove a song from the destination folder
-    #TODO transfer songs to the destination folder (with almost all existing songs) to check if the previous removed song was copied
-    #TODO transfer playlists files
-    #TODO remove the destination folder
-    a = 1
+from songstransfer import *
+
+"""def test_Transfer():
+	#TODO transfer songs to a new destination folder
+	#TODO remove a song from the destination folder
+	#TODO transfer songs to the destination folder (with almost all existing songs) to check if the previous removed song was copied
+	#TODO transfer playlists files
+	#TODO remove the destination folder"""
+
+# Save the ubication of the .xml archive with the iTunes library to export
+librarylocation = "./Test/iTunes Music Library.xml"
+
+# Save the ubication where the library want to import
+archivefinallocation = "./destination"
+
+# Original archive ubication
+originalarchivelocation = "./Test/Music"
+
+#songsTransferer(librarylocation, archivefinallocation, originalarchivelocation)
+
+playlistTransferer(librarylocation, archivefinallocation)
