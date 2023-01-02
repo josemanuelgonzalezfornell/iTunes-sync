e706b35ded2ddabd9fc467ff9af4fe5c16155ea1
Jose Manuel González Fornell <josemanuelgonzalezfornell@gmail.com>
2023-01-02 19:40:04 +0100

    Optimized method playlistTransferer()

--- songstransfer.py
+++ songstransfer.py
@@ -36,7 +36,7 @@ def songsTransferer(librarylocation, filefinallocation, originalfilelocation):
         folder = "/" + artist + "/" + album
 
         # create the file path
-        file = "/" + folder + "/"
+        file = folder + "/"
         if songs.discnumber is not None:
             file += str(songs.discnumber)+"-"
         file += tracknumber+" "+title+".mp3"
@@ -65,56 +65,42 @@ def playlistTransferer(librarylocation, archivefinallocation):
     # iTunes Library to export
     library = Library(librarylocation)
 
-    for songs in library.songs:
+    # sync playlist
+    for playlist in library.playlists:
+        for song in playlist.getSongs():
+			
+            # artist without special characters
+            artist = sub(specialcharacters, "_", song.artist)
+            # title without special characters
+            title = sub(specialcharacterstitle, "_", song.title)
+            # album without special characters
+            album = sub(specialcharacters, "_", song.album)
 
-        # artist without special characters
-        artist = sub(specialcharacters, "_", songs.artist)
-        # title without special characters
-        title = sub(specialcharacterstitle, "_", songs.title)
-        # album without special characters
-        album = sub(specialcharacters, "_", songs.album)
+            # Create a trucknumber with 2 figures
+            tracknumber = ""
+            if song.tracknumber < 10:
+                tracknumber += "0"
+            tracknumber += str(song.tracknumber)
 
-        # Create a trucknumber with 2 figures
-        tracknumber = ""
-        if songs.tracknumber < 10:
-            tracknumber += "0"
-        tracknumber += str(songs.tracknumber)
+            # create the folder tree
+            folder = "/" + artist + "/" + album
 
-        # create the folder tree
-        folder = "/" + artist + "/" + album
+            # create the file path
+            file = folder + "/"
+            if song.discnumber is not None:
+                file += str(song.discnumber)+"-"
+            file += tracknumber+" "+title+".mp3"
 
-        # create the file path
-        file = "/" + folder + "/"
-        if songs.discnumber is not None:
-            file += str(songs.discnumber)+"-"
-        file += tracknumber+" "+title+".mp3"
+            # create a .m3u file if it doesn't exist and add the song's path to this file
+            if exists(archivefinallocation + "/" + playlist.name + ".m3u") == False:
+                newplaylist = open(archivefinallocation + "/" +
+                                   playlist.name + ".m3u", "w")
+                newplaylist.write(file + "\n")
+                newplaylist.close()
 
-        # name of song playlist
-        nameplaylist = ""
-
-        # cover all the song pllaylists
-        for x in library.playlists:
-            index = 0
-            while index <= x.getLength()-1:
-                if x.getSongs()[index].id == songs.id:
-                    nameplaylist = x.name
-                index += 1
-
-            # enter in this condiction if song belong to a playlist
-            if nameplaylist != None:
-
-                # create a .m3u file if it doesn't exist and add the song's path to this file
-                if exists(archivefinallocation + "/" + nameplaylist + ".m3u") == False:
-                    newplaylist = open(archivefinallocation + "/" +
-                                       nameplaylist + ".m3u", "a")
-                    newplaylist.write(archivefinallocation + file + "\n")
-                    newplaylist.close()
-                    nameplaylist = ""
-
-                # add the song's path to the playlist file if it's exist
-                else:
-                    newplaylist = open(archivefinallocation + "/" +
-                                       nameplaylist + ".m3u", "a")
-                    newplaylist.write(archivefinallocation + file + "\n")
-                    newplaylist.close()
-                    nameplaylist = ""
+            # add the song's path to the playlist file if it's exist
+            else:
+                newplaylist = open(archivefinallocation + "/" +
+                                   playlist.name + ".m3u", "a")
+                newplaylist.write(file + "\n")
+                newplaylist.close()
